3533a9d5ae31f96f1293e1d91a552786d2cb75b3
Zhao Kang <1003994970@qq.com>
2015-12-22 20:21:27 +0800

    test

--- samples/socket/udp_client.py
+++ samples/socket/udp_client.py
@@ -2,12 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-for data in [b'Michael', b'Tracy', b'Sarah']:
+for data in [b'AAA', b'BBB', b'CCC']:
+
     # 发送数据:
+    for x in range(1,5):
+    	time.sleep(2)
+    	print(x, "...")
+    
     s.sendto(data, ('127.0.0.1', 9999))
+
     # 接收数据:
     print(s.recv(1024).decode('utf-8'))
 
--- samples/socket/udp_server.py
+++ samples/socket/udp_server.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -13,6 +14,15 @@ print('Bind UDP on 9999...')
 while True:
     # 接收数据:
     data, addr = s.recvfrom(1024)
+    for x in range(1,5):
+    	time.sleep(2)
+    	print(x, "...")
+    print(data)
+    print(addr)
     print('Received from %s:%s.' % addr)
+
+    for x in range(1,5):
+    	time.sleep(2)
+    	print(x, "...")
     reply = 'Hello, %s!' % data.decode('utf-8')
     s.sendto(reply.encode('utf-8'), addr)
